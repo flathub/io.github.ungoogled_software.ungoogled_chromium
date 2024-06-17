2e880dc850b097b396eae5abadb4fec640ddee4d
rany <ranygh@riseup.net>
2024-06-17 20:31:23 +0300

    Add dependency check and always print to stderr
    Signed-off-by: rany <ranygh@riseup.net>

--- maint/update_version.sh
+++ maint/update_version.sh
@@ -6,11 +6,31 @@ set -e
 # Fail on unset variables
 set -u
 
+# Helper function to print to stderr
+echoerr() {
+    printf "%s\n" "${*}" >&2
+}
+
+# Check for required tools
+DEPS="jq git curl base64 sha256sum grep awk cut"
+for dep in ${DEPS:?}; do
+    if ! command -v "${dep:?}" >/dev/null; then
+        echoerr "Error: ${dep:?} is not installed"
+        exit 1
+    fi
+done
+
+# Make sure we're using GNU grep
+if ! grep --version 2>/dev/null | grep -q 'GNU grep'; then
+    echoerr "Error: GNU grep is required"
+    exit 1
+fi
+
 # UGC version is {chromium_version}-{ugc_revision}
 ugc_version=${1:?}
 chromium_version=${ugc_version%-*}
-echo "UGC version: ${ugc_version:?}"
-echo "Chromium version: ${chromium_version:?}"
+echoerr "UGC version: ${ugc_version:?}"
+echoerr "Chromium version: ${chromium_version:?}"
 
 # Create a new branch from the latest master
 git fetch origin master
@@ -20,31 +40,31 @@ git checkout -b "update-ugc-${ugc_version:?}" origin/master
 clang_update_script=$(curl -L -s -f "https://chromium.googlesource.com/chromium/src/+/${chromium_version}/tools/clang/scripts/update.py?format=TEXT" | base64 -d)
 clang_version=$(printf %s "${clang_update_script:?}" | grep -oP "CLANG_REVISION = '(.*?)'" | cut -d"'" -f2) # llvmorg-19-init-9433-g76ea5feb
 clang_subversion=$(printf %s "${clang_update_script:?}" | grep -oP "CLANG_SUB_REVISION = (\d+)" | cut -d' ' -f3) # 1
-echo "Clang version: ${clang_version:?}"
-echo "Clang subversion: ${clang_subversion:?}"
+echoerr "Clang version: ${clang_version:?}"
+echoerr "Clang subversion: ${clang_subversion:?}"
 
 # Extract the SHA256 hash of the Clang x64 tarball
 clang_tarball_url="https://commondatastorage.googleapis.com/chromium-browser-clang/Linux_x64/clang-${clang_version}-${clang_subversion}.tar.xz"
 clang_sha256=$(curl -L -s -f "${clang_tarball_url:?}" | sha256sum | cut -d' ' -f1)
-echo "Clang tarball URL: ${clang_tarball_url:?}"
-echo "Clang tarball SHA256: ${clang_sha256:?}"
+echoerr "Clang tarball URL: ${clang_tarball_url:?}"
+echoerr "Clang tarball SHA256: ${clang_sha256:?}"
 
 # Get the Chromium SHA256 hash
 chromium_url="https://commondatastorage.googleapis.com/chromium-browser-official/chromium-${chromium_version}.tar.xz"
 chromium_sha256=$(curl -L -s -f "${chromium_url:?}.hashes" | grep -iE '^sha256\s+' | awk '{print $2}')
-echo "Chromium URL: ${chromium_url:?}"
-echo "Chromium SHA256: ${chromium_sha256:?}"
+echoerr "Chromium URL: ${chromium_url:?}"
+echoerr "Chromium SHA256: ${chromium_sha256:?}"
 
 # Get the UGC tag and commit
 ugc_url=$(jq -r '.[0].url' sources/ugc.json)
 ugc_tag=${ugc_version:?}
 ugc_commit=$(git ls-remote --tags "${ugc_url:?}" "refs/tags/${ugc_tag:?}" | cut -f1)
-echo "UGC tag: ${ugc_tag:?}"
-echo "UGC commit: ${ugc_commit:?}"
+echoerr "UGC tag: ${ugc_tag:?}"
+echoerr "UGC commit: ${ugc_commit:?}"
 
 # Get the Rust Nightly version
 rust_nightly_version=$(curl -L -s -f "https://static.rust-lang.org/dist/channel-rust-nightly.toml" | grep -oP 'date = "(.*?)"' | cut -d'"' -f2)
-echo "Rust Nightly version: ${rust_nightly_version:?}"
+echoerr "Rust Nightly version: ${rust_nightly_version:?}"
 url_aarch64="https://static.rust-lang.org/dist/${rust_nightly_version:?}/rust-nightly-aarch64-unknown-linux-gnu.tar.xz"
 sha256_aarch64=$(curl -L -s -f "${url_aarch64:?}.sha256" | cut -d' ' -f1)
 url_x86_64="https://static.rust-lang.org/dist/${rust_nightly_version:?}/rust-nightly-x86_64-unknown-linux-gnu.tar.xz"
@@ -94,18 +114,18 @@ for commit in ${commits_to_consider}; do
     commit_hash=$(printf '%s\n' "${commit}" | awk '{print $2}')
     commit_subject=$(printf '%s\n' "${commit}" | cut -d' ' -f3-)
     while true; do
-        echo "Apply commit ${commit_hash}: ${commit_subject}? [y/N] "
+        echoerr "Apply commit ${commit_hash}: ${commit_subject}? [y/N] "
         read -r REPLY || true
         if [ "${REPLY}" = "y" ] || [ "${REPLY}" = "Y" ]; then
             git cherry-pick -xs "${commit_hash:?}" || true
-            echo "Dropped to shell. Press Ctrl+D to continue cherry-picking."
+            echoerr "Dropped to shell. Press Ctrl+D to continue cherry-picking."
             "${SHELL:-/bin/sh}" || true
             break
         elif [ "${REPLY}" = "n" ] || [ "${REPLY}" = "N" ] || [ -z "${REPLY}" ]; then
             break
         fi
     done
-    echo "${commit_hash:?}" > maint/.org.chromium.Chromium.last_checked_commit
+    printf '%s\n' "${commit_hash:?}" > maint/.org.chromium.Chromium.last_checked_commit
 done
 
 # Pop the stashed changes
