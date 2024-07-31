flathub/io.github.ungoogled_software.ungoogled_chromium

commit 6808d09ae3064121b7f1f647252fa5432fb514b5

Move Ungoogled Chromium patch application to a seperate script
Also add it to the Chromium sources instead of installing to /app/ugc
and consistently use "UC" instead of "UGC" as the latter is no longer
encouraged.

Signed-off-by: rany <ranygh@riseup.net>

maint/update_version.sh
@@ -38,16 +38,16 @@ if ! grep --version 2>/dev/null | grep -q 'GNU grep'; then
     exit 1
 fi
 
-# UGC version is {chromium_version}-{ugc_revision}-{fp_revision}
-ugc_version=${1:?}
-chromium_version=${ugc_version%-*} # Remove the FP revision
-chromium_version=${chromium_version%-*} # Remove the UGC revision
-echoerr "UGC version: ${ugc_version:?}"
+# UC version is {chromium_version}-{uc_revision}-{fp_revision}
+uc_version=${1:?}
+chromium_version=${uc_version%-*} # Remove the FP revision
+chromium_version=${chromium_version%-*} # Remove the Ungoogled Chromium revision
+echoerr "Ungoogled Chromium version: ${uc_version:?}"
 echoerr "Chromium version: ${chromium_version:?}"
 
 # Create a new branch from the latest master
 git fetch origin master
-git checkout -b "update-ugc-${ugc_version:?}" origin/master
+git checkout -b "update-uc-${uc_version:?}" origin/master
 
 # Extract the LLVM version from the update.py script
 clang_update_script=$(curl -L -s -f "https://chromium.googlesource.com/chromium/src/+/${chromium_version}/tools/clang/scripts/update.py?format=TEXT" | base64 -d)
@@ -68,12 +68,12 @@ chromium_sha256=$(curl -L -s -f "${chromium_url:?}.hashes" | grep -iE '^sha256\s
 echoerr "Chromium URL: ${chromium_url:?}"
 echoerr "Chromium SHA256: ${chromium_sha256:?}"
 
-# Get the UGC tag and commit
-ugc_url=$(jq -r '.[0].url' sources/ugc.json)
-ugc_tag=${ugc_version:?}
-ugc_commit=$(git ls-remote --tags "${ugc_url:?}" "refs/tags/${ugc_tag:?}" | cut -f1)
-echoerr "UGC tag: ${ugc_tag:?}"
-echoerr "UGC commit: ${ugc_commit:?}"
+# Get the Ungoogled Chromium tag and commit
+uc_url=$(jq -r '.[0].url' sources/ungoogled-chromium.json)
+uc_tag=${uc_version:?}
+uc_commit=$(git ls-remote --tags "${uc_url:?}" "refs/tags/${uc_tag:?}" | cut -f1)
+echoerr "uc tag: ${uc_tag:?}"
+echoerr "uc commit: ${uc_commit:?}"
 
 # Get the Rust Nightly version
 rust_nightly_version=$(curl -L -s -f "https://static.rust-lang.org/dist/channel-rust-nightly.toml" | grep -oP 'date = "(.*?)"' | cut -d'"' -f2)
@@ -96,11 +96,11 @@ jq --arg clang_version "${clang_version}" \
     .[3] |= (.commit = $clang_version)' sources/chromium.json > sources/chromium.json.tmp
 mv sources/chromium.json.tmp sources/chromium.json
 
-# Update the UGC version
-jq --arg ugc_tag "${ugc_tag}" \
-   --arg ugc_commit "${ugc_commit}" \
-   '.[0] |= (.tag = $ugc_tag | .commit = $ugc_commit)' sources/ugc.json > sources/ugc.json.tmp
-mv sources/ugc.json.tmp sources/ugc.json
+# Update the Ungoogled Chromium version
+jq --arg uc_tag "${uc_tag}" \
+   --arg uc_commit "${uc_commit}" \
+   '.[0] |= (.tag = $uc_tag | .commit = $uc_commit)' sources/ungoogled-chromium.json > sources/ungoogled-chromium.json.tmp
+mv sources/ungoogled-chromium.json.tmp sources/ungoogled-chromium.json
 
 # Update the Rust Nightly version
 jq --arg rust_nightly_version "${rust_nightly_version}" \
@@ -116,7 +116,7 @@ jq --arg rust_nightly_version "${rust_nightly_version}" \
 mv sources/rust-nightly.json.tmp sources/rust-nightly.json
 
 # Stash the changes
-git stash push -m "Update Ungoogled Chromium to ${ugc_version:?}"
+git stash push -m "Update Ungoogled Chromium to ${uc_version:?}"
 
 # Fetch org.chromium.Chromium and offer to cherry-pick commits
 git remote add org.chromium.Chromium https://github.com/flathub/org.chromium.chromium 2>/dev/null || true
@@ -150,11 +150,11 @@ git stash pop || true
 # Create a new commit and push the changes
 git add \
     sources/chromium.json \
-    sources/ugc.json \
+    sources/ungoogled-chromium.json \
     sources/rust-nightly.json \
     maint/.org.chromium.Chromium.last_checked_commit
-git commit -s -m "Update Ungoogled Chromium to ${ugc_version:?}" || true
-git push origin "update-ugc-${ugc_version:?}"
+git commit -s -m "Update Ungoogled Chromium to ${uc_version:?}" || true
+git push origin "update-uc-${uc_version:?}"
 
 # Exit with success
 exit 0

build-aux/apply-uc-patches.sh
@@ -1,0 +1,14 @@
+#!/bin/bash -ex
+
+# Apply Flatpak specific branding
+./uc/branding/infect.sh ./uc ./
+
+# Prune binaries but keep Google toolchain binaries
+./uc/src/utils/prune_binaries.py --keep-contingent-paths ./ ./uc/src/pruning.list
+
+# Apply patches
+./uc/src/utils/patches.py apply ./ ./uc/src/patches
+
+# Substitute domains
+./uc/src/utils/domain_substitution.py apply -r ./uc/src/domain_regex.list \
+    -f ./uc/src/domain_substitution.list -c ./domsubcache.tar.gz ./

build-aux/bootstrap.sh
@@ -26,7 +26,7 @@ fi
 
 # (TODO: enable use_qt in the future?)
 mkdir -p out/Release
-cp /app/ugc/src/flags.gn out/Release/args.gn
+cp ./uc/src/flags.gn out/Release/args.gn
 cat >> out/Release/args.gn <<-EOF
 	use_sysroot=false
 	use_lld=true

build-aux/install.sh
@@ -18,6 +18,6 @@ for size in 24 48 64 128 256; do
 done
 install -Dvm 644 chrome/app/theme/chromium/product_logo.svg /app/share/icons/hicolor/scalable/apps/io.github.ungoogled_software.ungoogled_chromium.svg
 install -Dvm 644 cobalt.ini -t /app/etc
-install -Dvm 644 /app/ugc/io.github.ungoogled_software.ungoogled_chromium.desktop -t /app/share/applications
-install -Dvm 644 /app/ugc/io.github.ungoogled_software.ungoogled_chromium.metainfo.xml -t /app/share/metainfo
+install -Dvm 644 ./uc/io.github.ungoogled_software.ungoogled_chromium.desktop -t /app/share/applications
+install -Dvm 644 ./uc/io.github.ungoogled_software.ungoogled_chromium.metainfo.xml -t /app/share/metainfo
 install -Dvm 755 chromium.sh /app/bin/chromium

io.github.ungoogled_software.ungoogled_chromium.yaml
@@ -115,13 +115,6 @@ modules:
         url: https://github.com/google/material-design-icons/raw/f7bd4f25f3764883717c09a1fd867f560c9a9581/font/MaterialIconsTwoTone-Regular.otf
         sha256: 03ccf13774af5b5cbbad641dfa33038d4e9f4b7979eedc2c0a15f06bf9eb33cd
 
-  - name: ungoogled-chromium
-    buildsystem: simple
-    build-commands:
-      - cp -r "${PWD}" /app/ugc
-    sources:
-      - sources/ugc.json
-
   - name: rust-nightly
     buildsystem: simple
     build-options:
@@ -142,16 +135,12 @@ modules:
     build-options:
       prepend-ld-library-path: /app/lib/sdk/rust-nightly/lib
     build-commands:
-      - /app/ugc/branding/infect.sh /app/ugc ./
-      - /app/ugc/src/utils/prune_binaries.py --keep-contingent-paths ./ /app/ugc/src/pruning.list
-      - /app/ugc/src/utils/patches.py apply ./ /app/ugc/src/patches
-      - /app/ugc/src/utils/domain_substitution.py apply -r /app/ugc/src/domain_regex.list
-        -f /app/ugc/src/domain_substitution.list -c ./domsubcache.tar.gz ./
+      - ./apply-uc-patches.sh
       - ./bootstrap.sh
       - ./build.sh
       - ./install.sh
-      - rm -rf /app/ugc
     sources:
+      - sources/ungoogled-chromium.json
       - sources/chromium.json
       - patches/chromium/_sources.json
       - patches/ffmpeg/_sources.json

commit d82dcc4575a139b5c58301e8d3682afe2b4aafef

Fix maint typo caused by renaming all references of "UGC" to "UC"
This became lowercased by accident. No functional changes.

Fixes: 6808d09 ("Move Ungoogled Chromium patch application to a seperate script")
Signed-off-by: rany <ranygh@riseup.net>

maint/update_version.sh
@@ -72,8 +72,8 @@ echoerr "Chromium SHA256: ${chromium_sha256:?}"
 uc_url=$(jq -r '.[0].url' sources/ungoogled-chromium.json)
 uc_tag=${uc_version:?}
 uc_commit=$(git ls-remote --tags "${uc_url:?}" "refs/tags/${uc_tag:?}" | cut -f1)
-echoerr "uc tag: ${uc_tag:?}"
-echoerr "uc commit: ${uc_commit:?}"
+echoerr "Ungoogled Chromium tag: ${uc_tag:?}"
+echoerr "Ungoogled Chromium commit: ${uc_commit:?}"
 
 # Get the Rust Nightly version
 rust_nightly_version=$(curl -L -s -f "https://static.rust-lang.org/dist/channel-rust-nightly.toml" | grep -oP 'date = "(.*?)"' | cut -d'"' -f2)