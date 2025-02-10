318be637aac875c167016672239bfbf993bbe087
rany <ranygh@riseup.net>
2025-02-10 14:36:01 +0000

    Switch to system Clang and Rust
    Signed-off-by: rany <ranygh@riseup.net>

--- build-aux/bootstrap.sh
+++ build-aux/bootstrap.sh
@@ -1,59 +1,39 @@
 #!/bin/bash -ex
 
 # Needed to build GN itself.
-. /usr/lib/sdk/llvm18/enable.sh
+. /usr/lib/sdk/llvm19/enable.sh
 
 # GN will use these variables to configure its own build, but they introduce
 # compat issues w/ Clang and aren't used by Chromium itself anyway, so just
 # unset them here.
 unset CFLAGS CXXFLAGS LDFLAGS
 
-chrome_pgo_phase=2
-# Support the Gentoo Chromium tarballs, which are missing the full GN source.
-if [[ ! -f tools/gn/build/gen.py ]]; then
-	chrome_pgo_phase=0
-	cp -r tools/gn.git/* tools/gn
-	cat > tools/gn/bootstrap/last_commit_position.h <<-EOF
-	#ifndef OUT_LAST_COMMIT_POSITION_H_
-	#define OUT_LAST_COMMIT_POSITION_H_
-	#define LAST_COMMIT_POSITION_NUM 1
-	#define LAST_COMMIT_POSITION "unknown"
-	#endif
-	EOF
-fi
+# Allow building against system libraries in official builds
+sed -i 's/OFFICIAL_BUILD/GOOGLE_CHROME_BUILD/' \
+	tools/generate_shim_headers/generate_shim_headers.py
 
-if [[ -d third_party/llvm-build/Release+Asserts/bin ]]; then
-	# The build scripts check that the stamp file is present, so write it out
-	# here.
-	PYTHONPATH=tools/clang/scripts/ \
-		python3 -c 'import update; print(update.PACKAGE_VERSION)' \
-		> third_party/llvm-build/Release+Asserts/cr_build_revision
-else
-	python3 tools/clang/scripts/build.py --disable-asserts --pic \
-		--skip-checkout --use-system-cmake --use-system-libxml \
-		--host-cc=/usr/lib/sdk/llvm18/bin/clang \
-		--host-cxx=/usr/lib/sdk/llvm18/bin/clang++ \
-		--target-triple=$(clang -dumpmachine) \
-		--without-android --without-fuchsia --without-zstd \
-		--with-ml-inliner-model=
-fi
+# https://crbug.com/893950
+sed -i -e 's/\<xmlMalloc\>/malloc/' -e 's/\<xmlFree\>/free/' \
+	-e '1i #include <cstdlib>' \
+	third_party/blink/renderer/core/xml/*.cc \
+	third_party/blink/renderer/core/xml/parser/xml_document_parser.cc \
+	third_party/libxml/chromium/*.cc
 
+# Required so that rust-bindgen can find libclang
+# https://rust-lang.github.io/rust-bindgen/requirements.html
 cp -r /app/lib/sdk/bindgen bindgen
-if [[ -e third_party/rust-toolchain/lib/libclang.so ]]; then
-  ln -s "$PWD/third_party/rust-toolchain/lib" -t bindgen
-else
-  ln -s "$PWD/third_party/llvm-build/Release+Asserts/lib" -t bindgen
-fi
+ln -s /usr/lib/sdk/llvm19/lib -t bindgen
 
 # (TODO: enable use_qt in the future?)
 mkdir -p out/Release
 cp ./uc/src/flags.gn out/Release/args.gn
 cat >> out/Release/args.gn <<-EOF
+	custom_toolchain="//build/toolchain/linux/unbundle:default"
+	host_toolchain="//build/toolchain/linux/unbundle:default"
 	use_sysroot=false
 	use_lld=true
 	enable_nacl=false
 	blink_symbol_level=0
-	use_gnome_keyring=false
 	use_pulseaudio=true
 	clang_use_chrome_plugins=false
 	is_official_build=true
@@ -70,15 +50,12 @@ cat >> out/Release/args.gn <<-EOF
 	use_system_libffi=true
 	use_qt=false
 	enable_remoting=false
-	rust_sysroot_absolute="/app/lib/sdk/rust-nightly"
-	rustc_version="$(/app/lib/sdk/rust-nightly/bin/rustc -V)"
-	rust_bindgen_root="$PWD/bindgen"
-	chrome_pgo_phase=${chrome_pgo_phase}
+	clang_base_path="/usr/lib/sdk/llvm19"
+	clang_use_chrome_plugins=false
+	clang_version="$(clang --version | grep -m1 version | sed 's/.* \([0-9]\+\).*/\1/')"
+	chrome_pgo_phase=2
+	rust_sysroot_absolute="/usr/lib/sdk/rust-stable"
+	rust_bindgen_root="${PWD}/bindgen"
+	rustc_version="$(/usr/lib/sdk/rust-stable/bin/rustc -V)"
 EOF
 tools/gn/bootstrap/bootstrap.py -v --no-clean --skip-generate-buildfiles -j"${FLATPAK_BUILDER_N_JOBS}"
-
-mkdir -p out/ReleaseFree
-cp out/Release{,Free}/args.gn
-echo -e 'proprietary_codecs = false\nffmpeg_branding = "Chromium"' >> out/ReleaseFree/args.gn
-out/Release/gn gen out/Release
-out/Release/gn gen out/ReleaseFree
--- build-aux/build.sh
+++ build-aux/build.sh
@@ -11,5 +11,51 @@ ln_overwrite_all() {
 ln_overwrite_all /usr/lib/sdk/node20 third_party/node/linux/node-linux-x64
 ln_overwrite_all /usr/lib/sdk/openjdk21 third_party/jdk/current
 
+# Use system clang
+. /usr/lib/sdk/llvm19/enable.sh
+export CC=clang
+export CXX=clang++
+export AR=ar
+export NM=nm
+
+# Allow the use of nightly features with stable Rust compiler
+# https://github.com/ungoogled-software/ungoogled-chromium/pull/2696#issuecomment-1918173198
+export RUSTC_BOOTSTRAP=1
+
+# Facilitate deterministic builds (taken from build/config/compiler/BUILD.gn)
+CFLAGS+='   -Wno-builtin-macro-redefined'
+CXXFLAGS+=' -Wno-builtin-macro-redefined'
+CPPFLAGS+=' -D__DATE__=  -D__TIME__=  -D__TIMESTAMP__='
+
+# Do not warn about unknown warning options
+CFLAGS+='   -Wno-unknown-warning-option'
+CXXFLAGS+=' -Wno-unknown-warning-option'
+
+# Let Chromium set its own symbol level
+CFLAGS=${CFLAGS/-g }
+CXXFLAGS=${CXXFLAGS/-g }
+
+# https://github.com/ungoogled-software/ungoogled-chromium-archlinux/issues/123
+CFLAGS=${CFLAGS/-fexceptions}
+CFLAGS=${CFLAGS/-fcf-protection}
+CXXFLAGS=${CXXFLAGS/-fexceptions}
+CXXFLAGS=${CXXFLAGS/-fcf-protection}
+
+# This appears to cause random segfaults when combined with ThinLTO
+# https://bugs.archlinux.org/task/73518
+CFLAGS=${CFLAGS/-fstack-clash-protection}
+CXXFLAGS=${CXXFLAGS/-fstack-clash-protection}
+
+# https://crbug.com/957519#c122
+CXXFLAGS=${CXXFLAGS/-Wp,-D_GLIBCXX_ASSERTIONS}
+
+# Configuring Chromium
+mkdir -p out/ReleaseFree
+cp out/Release{,Free}/args.gn
+echo -e 'proprietary_codecs = false\nffmpeg_branding = "Chromium"' >> out/ReleaseFree/args.gn
+out/Release/gn gen out/Release
+out/Release/gn gen out/ReleaseFree
+
+# Building Chromium
 ninja -C out/ReleaseFree -j"${FLATPAK_BUILDER_N_JOBS}" libffmpeg.so
 ninja -C out/Release -j"${FLATPAK_BUILDER_N_JOBS}" chrome chrome_crashpad_handler
