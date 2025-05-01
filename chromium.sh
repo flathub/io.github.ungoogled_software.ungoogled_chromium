4be5dcd741e33e96fdce6a0740bfadab1122a8ca
rany <rany2@riseup.net>
2025-05-01 10:25:46 +0300

    Handle shellcheck complaints for chromium.sh
    Signed-off-by: rany <rany2@riseup.net>

--- chromium.sh
+++ chromium.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 merge_extensions() {
-  (
-    shopt -s nullglob
-    dest=/app/chromium/extensions/$1
-    mkdir -p $dest
-    for ext in /app/chromium/${1%/*}/$1/*; do
-      ln -s $ext $dest
-    done
-  )
+	(
+		shopt -s nullglob
+		dest="/app/chromium/extensions/$1"
+		mkdir -p "${dest}"
+		for ext in "/app/chromium/${1%/*}/$1/"*; do
+			ln -s "${ext}" "${dest}"
+		done
+	)
 }
 
-if [[ ! -f /app/chromium/extensions/no-mount-stamp ]]; then
-  # Merge all legacy extension points if the symlinks had a tmpfs mounted over
-  # them.
-  merge_extensions native-messaging-hosts
-  merge_extensions policies/managed
-  merge_extensions policies/recommended
+if [[ ! -f "/app/chromium/extensions/no-mount-stamp" ]]; then
+	# Merge all legacy extension points if the symlinks
+	# had a tmpfs mounted over them.
+	merge_extensions "native-messaging-hosts"
+	merge_extensions "policies/managed"
+	merge_extensions "policies/recommended"
 fi
 
 exec cobalt "$@"
