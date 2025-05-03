5614d41cde6601510d9ede82ba105412e8c27d99
rany <rany2@riseup.net>
2025-05-03 09:45:44 +0300

    Reduce amount of copied files into the final install
    Use the file list based on the Arch listing instead. This should
    help reduce the final install size, especially with the elimation
    of *.info files in locales.
    
    Signed-off-by: rany <rany2@riseup.net>

--- build-aux/install.sh
+++ build-aux/install.sh
@@ -3,19 +3,37 @@ set -exo pipefail
 
 mkdir -p /app/chromium
 
-pushd out/Release
-for path in chrome chrome_crashpad_handler icudtl.dat *.so libvulkan.so.1 *.pak *.bin *.png locales MEIPreload vk_swiftshader_icd.json; do
-	# All the 'libVk*' names are just for debugging, stuff like "libVkICD_mock_icd" and "libVkLayer_khronos_validation".
-	[[ "${path}" == libVk* ]] && continue
-	cp -rv "${path}" /app/chromium || true
-done
-popd
+TOPLEVEL_FILES=(
+	chrome
+	chrome_100_percent.pak
+	chrome_200_percent.pak
+	chrome_crashpad_handler
+	resources.pak
+	v8_context_snapshot.bin
+
+	# ANGLE
+	libEGL.so
+	libGLESv2.so
+
+	# SwiftShader ICD
+	libvk_swiftshader.so
+	libvulkan.so.1
+	vk_swiftshader_icd.json
+)
+
+cp -v "${TOPLEVEL_FILES[@]/#/out/Release/}" /app/chromium/
+install -Dvm644 -t /app/chromium/locales out/Release/locales/*.pak
 
 for size in 24 48 64 128 256; do
-	install -Dvm 644 "chrome/app/theme/chromium/product_logo_${size}.png" "/app/share/icons/hicolor/${size}x${size}/apps/io.github.ungoogled_software.ungoogled_chromium.png";
+	install -Dvm644 "chrome/app/theme/chromium/product_logo_${size}.png" "/app/share/icons/hicolor/${size}x${size}/apps/io.github.ungoogled_software.ungoogled_chromium.png"
 done
-install -Dvm 644 chrome/app/theme/chromium/product_logo.svg /app/share/icons/hicolor/scalable/apps/io.github.ungoogled_software.ungoogled_chromium.svg
-install -Dvm 644 cobalt.ini -t /app/etc
-install -Dvm 644 io.github.ungoogled_software.ungoogled_chromium.desktop -t /app/share/applications
-install -Dvm 644 io.github.ungoogled_software.ungoogled_chromium.metainfo.xml -t /app/share/metainfo
-install -Dvm 755 chromium.sh /app/bin/chromium
+
+for size in 16 32; do
+	install -Dvm644 "chrome/app/theme/default_100_percent/chromium/product_logo_${size}.png" "/app/share/icons/hicolor/${size}x${size}/apps/io.github.ungoogled_software.ungoogled_chromium.png"
+done
+
+install -Dvm644 chrome/app/theme/chromium/product_logo.svg /app/share/icons/hicolor/scalable/apps/io.github.ungoogled_software.ungoogled_chromium.svg
+install -Dvm644 cobalt.ini -t /app/etc
+install -Dvm644 io.github.ungoogled_software.ungoogled_chromium.desktop -t /app/share/applications
+install -Dvm644 io.github.ungoogled_software.ungoogled_chromium.metainfo.xml -t /app/share/metainfo
+install -Dvm755 chromium.sh /app/bin/chromium
