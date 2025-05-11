b5837dccf22fcf6ec5fc02021d13e5df8bf7d0e4
rany <rany2@riseup.net>
2025-05-11 17:59:21 +0300

    Misc cleanup
    Signed-off-by: rany <rany2@riseup.net>

--- build-aux/install.sh
+++ build-aux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -exo pipefail
+set -euxo pipefail
 
-mkdir -p /app/chromium
+mkdir -p "${FLATPAK_DEST}/chromium"
 
 TOPLEVEL_FILES=(
 	chrome
@@ -21,32 +21,32 @@ TOPLEVEL_FILES=(
 	vk_swiftshader_icd.json
 )
 
-cp -v "${TOPLEVEL_FILES[@]/#/out/Release/}" /app/chromium/
+cp -v "${TOPLEVEL_FILES[@]/#/out/Release/}" "${FLATPAK_DEST}/chromium/"
 
-mkdir -p /app/chromium/locales
+mkdir -p "${FLATPAK_DEST}/chromium/locales"
 for locale_path in out/Release/locales/*.pak; do
 	locale_file=${locale_path##*/}
 	lang_region=${locale_file%.pak}
 	lang_code=${lang_region%%-*}
 
-	dest_dir="/app/share/runtime/locale/${lang_code}"
+	dest_dir="${FLATPAK_DEST}/share/runtime/locale/${lang_code}"
 	dest_file="${dest_dir}/${locale_file}"
 
 	mkdir -p "${dest_dir}"
 	gzip -9nc "${locale_path}" > "${dest_file}"
-	ln -svf "${dest_file}" "/app/chromium/locales/${locale_file}"
+	ln -svf "${dest_file}" "${FLATPAK_DEST}/chromium/locales/${locale_file}"
 done
 
 for size in 24 48 64 128 256; do
-	install -Dvm644 "chrome/app/theme/chromium/product_logo_${size}.png" "/app/share/icons/hicolor/${size}x${size}/apps/io.github.ungoogled_software.ungoogled_chromium.png"
+	install -Dvm644 "chrome/app/theme/chromium/product_logo_${size}.png" "${FLATPAK_DEST}/share/icons/hicolor/${size}x${size}/apps/io.github.ungoogled_software.ungoogled_chromium.png"
 done
 
 for size in 16 32; do
-	install -Dvm644 "chrome/app/theme/default_100_percent/chromium/product_logo_${size}.png" "/app/share/icons/hicolor/${size}x${size}/apps/io.github.ungoogled_software.ungoogled_chromium.png"
+	install -Dvm644 "chrome/app/theme/default_100_percent/chromium/product_logo_${size}.png" "${FLATPAK_DEST}/share/icons/hicolor/${size}x${size}/apps/io.github.ungoogled_software.ungoogled_chromium.png"
 done
 
-install -Dvm644 chrome/app/theme/chromium/product_logo.svg /app/share/icons/hicolor/scalable/apps/io.github.ungoogled_software.ungoogled_chromium.svg
-install -Dvm644 cobalt.ini -t /app/etc
-install -Dvm644 io.github.ungoogled_software.ungoogled_chromium.desktop -t /app/share/applications
-install -Dvm644 io.github.ungoogled_software.ungoogled_chromium.metainfo.xml -t /app/share/metainfo
-install -Dvm755 chromium.sh /app/bin/chromium
+install -Dvm644 chrome/app/theme/chromium/product_logo.svg "${FLATPAK_DEST}/share/icons/hicolor/scalable/apps/io.github.ungoogled_software.ungoogled_chromium.svg"
+install -Dvm644 cobalt.ini -t "${FLATPAK_DEST}/etc"
+install -Dvm644 io.github.ungoogled_software.ungoogled_chromium.desktop -t "${FLATPAK_DEST}/share/applications"
+install -Dvm644 io.github.ungoogled_software.ungoogled_chromium.metainfo.xml -t "${FLATPAK_DEST}/share/metainfo"
+install -Dvm755 chromium.sh "${FLATPAK_DEST}/bin/chromium"
