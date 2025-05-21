1a1822cd01502045d00e5af500abaad307487bfb
rany <rany2@riseup.net>
2025-05-21 23:14:18 +0300

    Fix up style and add new make-new-release.sh script
    This should make it so that I don't forget to update the metainfo file.
    
    Signed-off-by: rany <rany2@riseup.net>

---
+++ make-new-release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage: $0 <version>"
+	exit 1
+fi
+
+VERSION="$1"
+DATE=$(date -u -I)
+METAFILE="io.github.ungoogled_software.ungoogled_chromium.metainfo.xml"
+COMMIT_MSG="Update Ungoogled Chromium to ${VERSION}"
+
+sed -i "/<releases>/a \\
+    <release version=\"${VERSION}\" date=\"${DATE}\" />" "${METAFILE}"
+git add "${METAFILE}"
+git commit -m "${COMMIT_MSG}"
