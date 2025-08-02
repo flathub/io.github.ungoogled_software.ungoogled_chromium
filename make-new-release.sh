9cffe86e4efe5fc570c69a71288877d68dd0f4c3
rany <rany2@riseup.net>
2025-08-02 18:13:44 +0300

    Update Ungoogled Chromium to 138.0.7204.183-1
    Signed-off-by: rany <rany2@riseup.net>

--- make-new-release.sh
+++ make-new-release.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-if [[ $# -ne 1 ]]; then
-	echo "Usage: $0 <version>"
+die() {
+	echo >&2 "$@"
 	exit 1
+}
+
+# shellcheck disable=SC2034
+SED_BINARIES=(gsed sed)
+
+if [[ $# -ne 1 ]]; then
+	die "Usage: $0 <version>"
 fi
 
 VERSION="$1"
@@ -11,7 +18,22 @@ DATE=$(date -u -I)
 METAFILE="io.github.ungoogled_software.ungoogled_chromium.metainfo.xml"
 COMMIT_MSG="Update Ungoogled Chromium to ${VERSION}"
 
-sed -i "/<releases>/a \\
+choose_and_run() {
+	local -n binaries="$1"
+	shift
+
+	local binary
+	for binary in "${binaries[@]}"; do
+		if command -v "${binary}" &> /dev/null; then
+			"${binary}" "$@"
+			return
+		fi
+	done
+
+	die "No suitable binary found for: ${binaries[*]}"
+}
+
+choose_and_run SED_BINARIES -i "/<releases>/a \\
     <release version=\"${VERSION}\" date=\"${DATE}\" />" "${METAFILE}"
 git add "${METAFILE}"
 git commit -sm "${COMMIT_MSG}"
