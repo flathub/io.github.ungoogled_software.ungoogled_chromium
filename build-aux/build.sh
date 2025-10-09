3673d6bfd7d8947bd0736e49cdc8c738d0f07bfb
rany <rany2@riseup.net>
2025-10-09 20:30:39 +0300

    Disable PGO when using ccache
    Signed-off-by: rany <rany2@riseup.net>

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -91,6 +91,7 @@ flags+=(
 # Use ccache if enabled by flatpak-builder
 if [[ "${CCACHE_DIR}" == "/run/ccache" ]]; then
 	flags+=('cc_wrapper = "ccache"')
+	flags+=('chrome_pgo_phase = 0')
 fi
 
 # Use VAAPI on x86_64 and V4L2 on aarch64
